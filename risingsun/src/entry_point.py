cf88e406766a44d98c29ee29f464416408c7fe93
Yash Lodaya <yashlodaya.18@gmail.com>
2022-07-26 16:34:59 +0530

    PUSH-3

--- risingsun/src/entry_point.py
+++ risingsun/src/entry_point.py
@@ -1,21 +1,23 @@
 import pandas
-
 import access_token
 import repository
 import pandas as pd
 
-STOCK_NAME = "M&M"
+STOCK_NAME = "SBIN"
 RESOLUTION = "5"
-FROM_DAYS = 10
-dema_time_period = 200
+FROM_DAYS = 1
+
+dema_time_period = 3
 supertrend_ATR = 12
 supertrend_Multiplier = 3
+per = 1.03 # sell percentage criteria
 
 
 OPEN_VALUES = []
 HIGH_VALUES = []
 LOW_VALUES = []
 CLOSE_VALUES = []
+EPOCH_VALUES = []
 
 # Generating Stock Details
 stock_meta_data = repository.pass_stock_data(STOCK_NAME, RESOLUTION, FROM_DAYS)
@@ -30,10 +32,12 @@ print("The length of the entire stock data is = ", len(entire_stock_data['candle
 # print("The entire stock detail is - \n ", entire_stock_data)
 
 for i in range(len((entire_stock_data)["candles"])):
+    EPOCH_VALUES.append((entire_stock_data)["candles"][i][0])
     OPEN_VALUES.append((entire_stock_data)["candles"][i][1])
     HIGH_VALUES.append((entire_stock_data)["candles"][i][2])
     LOW_VALUES.append((entire_stock_data)["candles"][i][3])
     CLOSE_VALUES.append((entire_stock_data)["candles"][i][4])
+
 
 dataframe_data = pd.DataFrame(
     {"HIGH": repository.list_to_numpy_array(HIGH_VALUES), "LOW": repository.list_to_numpy_array(LOW_VALUES),
@@ -46,55 +50,66 @@ buy_value = 0
 trade_counter = 0 # it will count sell orders
 trade_diff_result = []
 discrete_diff = 0
-print(repository.get_dema(repository.list_to_numpy_array(CLOSE_VALUES),dema_time_period))
+
+first_Epoch = (entire_stock_data)["candles"][0][0]
+print(first_Epoch)
+to_Epoch_range = first_Epoch + 3600
+print(to_Epoch_range)
+
 for i in range(len(CLOSE_VALUES)):
-    if CLOSE_VALUES[i] > repository.get_dema(repository.list_to_numpy_array(CLOSE_VALUES),
-                                             dema_time_period)[i] and \
-            repository.get_supertrend(dataframe_data, supertrend_ATR, supertrend_Multiplier)["Supertrend"][i] and a != 1:
-        print(f"{i + 1} pe Buy")
-        sum_buy += CLOSE_VALUES[i]
-        buy_value = CLOSE_VALUES[i]
-        buy_index = i
-        a = 1
-    elif (repository.get_supertrend(dataframe_data, supertrend_ATR, supertrend_Multiplier)["Supertrend"][i] == False or (buy_value * (1.04) <
-                                                                                                                         CLOSE_VALUES[i])) and a == 1:
-        print(f"{i + 1} pe Sell")
-        sum_sell += CLOSE_VALUES[i]
-        trade_counter = trade_counter + 1
-        sell_value = CLOSE_VALUES[i]
-        a = 0
-
-
-        buy_dict = {
-            "index": buy_index,
-            "value": buy_value
-        }
-
-        sell_dict = {
-            "index": i,
-            "value": sell_value
-        }
-
-        trade_diff = sell_value - buy_value
-        serial_number = trade_counter
-
-        temp_list = []
-        discrete_diff = trade_diff + discrete_diff
-
-        temp_list.append(serial_number)
-        temp_list.append(buy_dict)
-        temp_list.append(sell_dict)
-        temp_list.append(trade_diff)
-        temp_list.append(discrete_diff)
-
-        trade_diff_result.append(temp_list)
-
-print(pandas.DataFrame(trade_diff_result))
+    if(first_Epoch <= (entire_stock_data)["candles"][0][i] <= to_Epoch_range ):
 
 
 
+# for i in range(len(CLOSE_VALUES)):
+#     if CLOSE_VALUES[i] > repository.get_dema(repository.list_to_numpy_array(CLOSE_VALUES),
+#                                              dema_time_period)[i] and \
+#             repository.get_supertrend(dataframe_data, supertrend_ATR, supertrend_Multiplier)["Supertrend"][i] and a != 1:
+#         print("BUY @ UserIndex: ", (i+1), " SystemIndex: ", i)
+#         sum_buy += CLOSE_VALUES[i]
+#         buy_value = CLOSE_VALUES[i]
+#         buy_index = i
+#         a = 1
+#     elif (repository.get_supertrend(dataframe_data, supertrend_ATR, supertrend_Multiplier)["Supertrend"][i] == False or (buy_value * (per) <
+#                                                                                                                          CLOSE_VALUES[i])) and a == 1:
+#         print("SELL @ UserIndex: ", (i+1), " SystemIndex: ", i)
+#         sum_sell += CLOSE_VALUES[i]
+#         trade_counter = trade_counter + 1
+#         sell_value = CLOSE_VALUES[i]
+#         a = 0
+#
+#
+#         buy_dict = {
+#             "index": buy_index,
+#             "value": buy_value
+#         }
+#
+#         sell_dict = {
+#             "index": i,
+#             "value": sell_value
+#         }
+#
+#         trade_diff = sell_value - buy_value
+#         serial_number = trade_counter
+#
+#         temp_list = []
+#         discrete_diff = trade_diff + discrete_diff
+#
+#         temp_list.append(serial_number)
+#         temp_list.append(buy_dict)
+#         temp_list.append(sell_dict)
+#         temp_list.append(trade_diff)
+#         temp_list.append(discrete_diff)
+#
+#         trade_diff_result.append(temp_list)
+#
 
 
 
+
+
+
+# print(repository.get_dema(repository.list_to_numpy_array(CLOSE_VALUES),dema_time_period))
+# print(pandas.DataFrame(trade_diff_result).to_string())
 
 print("\n=================APP CLOSED=====================")
--- risingsun/src/experiments.py
+++
@@ -1,6 +0,0 @@
-list_main = []
-sub_list = []
-sub_list.
-list_main.append(sub_list)
-
-print(list_main)
